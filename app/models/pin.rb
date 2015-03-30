03c94eeb4ff675fb961d4c4e8b0419b324c849c9
Nechama Goldman <nechamabgoldman@gmail.com>
2015-03-30 16:42:53 -0400

    added image to view pin page

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,6 +4,8 @@ class Pin < ActiveRecord::Base
 	validates_presence_of :user_id #can only create a pin with a user
 
 	has_attached_file :image , :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+	do_not_validate_attachment_file_type :image
+
+	#validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
 end
